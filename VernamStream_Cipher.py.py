0453571263a1d34aee69e349a7776572a112b8f0
hazeem'yasseer <51271019+hazemyasr@users.noreply.github.com>
2020-03-01 23:33:17 +0200

    Update VernamStream_Cipher.py.py

--- VernamStream_Cipher.py.py
+++ VernamStream_Cipher.py.py
@@ -1,47 +1,40 @@
 while True:
-    ch =int(input("Press 1 to Encrypt || Press 2 to Decrypt\n"))
-
+    ch =int(input("Press 1 to Encrypt || Press 2 to Decrypt\n>>>"))
     if ch == 1:
         text = input("text:") 
-
+        print("!!! KEY MUST BE CHARACTERS !!!\n")
         key = input("Key:")  
 
         if len(key) != len(text):
-            key = key*2         
+            key = key*100         
         else:
             pass
 
         tx_ascii = [ord(tx) for tx in text]
-
         ky_ascii = [ord(ky) for ky in key]
 
         charactrize=[]
-
         for as1, as2 in zip(tx_ascii, ky_ascii): #@@@@@@
             charactrize.append(chr(as1 ^ as2)) 
 
-        vernam ="".join(charactrize) ##@@@@@@@
         print("================================================")
         print("             !!! Encrypted !!!                  ")
-        print("text:",vernam)
+        print("text:","".join(charactrize))
         print("================================================\n")
 
     elif ch == 2:
 
         text = input("text:") 
-
         key = input("Key:")  
 
         tx_ascii = [ord(tx) for tx in text]
-
         ky_ascii = [ord(ky) for ky in key]
 
         charactrize=[]
         for as1, as2 in zip(tx_ascii, ky_ascii): #@@@@@@
-            charactrize.append(chr(as1 ^ as2)) 
+            charactrize.append(chr(as2 ^ as1)) 
 
-        vernam ="".join(charactrize) #@@@@@
         print("================================================")
         print("             !!! Decrypted !!!                  ")
-        print("text:",vernam)
+        print("text:","".join(charactrize))
         print("================================================\n")
